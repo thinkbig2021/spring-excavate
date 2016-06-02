cf0a98952c9f73598274f416c7bc115b502721cc
yumeng <472827074@qq.com>
2016-06-02 23:17:05 +0800

    dsdaadad

---
+++ src/main/java/com/yumeng/spring/aop1/Test.java
@@ -0,0 +1,16 @@
+package com.yumeng.spring.aop1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=
+	            new ClassPathXmlApplicationContext("com/yumeng/spring/aop1/spring-context.xml");
+		TestAspect test =(TestAspect)ctx.getBean("testAspect");
+		//ctx.publishEvent(event);
+	   test.test1("yumeng");
+	}
+
+}
