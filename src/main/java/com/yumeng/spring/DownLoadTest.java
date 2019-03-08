56c857ff749df0ac17b16be05d3c60c770079fcf
余梦 <yumeng@dianwoba.com>
2019-03-08 09:16:54 +0800

    11

--- src/main/java/com/yumeng/spring/DownLoadTest.java
+++ src/main/java/com/yumeng/spring/DownLoadTest.java
@@ -5,7 +5,8 @@ public class DownLoadTest {
      * @param args
      */
     public static void main(String[] args) {
-        
+
+
         String filepath = "http://url.tudown.com/down/%E9%AD%94%E5%85%BD%E4%BA%89%E9%9C%B83%EF%BC%9A%E5%86%B0%E5%B0%81%E7%8E%8B%E5%BA%A71.20.0.0@83_950.exe";
         MultiTheradDownLoad load = new MultiTheradDownLoad(filepath ,2);    
         load.downloadPart();    
